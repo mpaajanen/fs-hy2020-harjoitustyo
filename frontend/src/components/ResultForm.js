dbca7b4fd1f5cae7e460f611f8b8651fc1d3bd68
mikkopaa <mikko.paajanen@metropolia.fi>
2021-06-16 14:42:58 +0300

    Tulosten dynaaminen syöttäminen onnistuu

--- frontend/src/components/ResultForm.js
+++ frontend/src/components/ResultForm.js
@@ -1,34 +1,75 @@
-import React from 'react'
-
-const handleSubmit = () => {
-  console.log('tulos lähetetty')
-}
-
-const handlePointChange1 = (event) => {
-  event.preventDefault
-  console.log('p1')
-}
-
-const handlePointChange2 = (event) => {
-  event.preventDefault
-  console.log('p2')
-}
+import React, { useState } from 'react'
+import TextField from '@material-ui/core/TextField'
+import IconButton from '@material-ui/core/IconButton'
+import DoneIcon from '@material-ui/icons/Done'
+import DeleteIcon from '@material-ui/icons/Delete'
 
 const ResultForm = () => {
+  const frameObj = {
+    p1: '',
+    p2: ''
+  }
+  const [pointFields, setPointFields] = useState([frameObj])
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log('tulos lähetetty', pointFields)
+  }
+
+  // const handleChange = (event) => {
+  //   event.preventDefault
+  //   console.log('p1')
+  // }
+
+  const handleChange = (index, event) => {
+    const newPointFields = pointFields.map((i, ind) => {
+      if(ind === index) {
+        i[event.target.name] = event.target.value
+      }
+      return i
+    })
+
+    setPointFields(newPointFields)
+  }
+
+  const handleAddFields = () => {
+    setPointFields([...pointFields, frameObj])
+  }
+
+  const handleRemoveFields = index => {
+    const values  = [...pointFields]
+    values.splice(index, 1)
+    setPointFields(values)
+  }
+
   return (
     <form onSubmit={event => handleSubmit(event)}>
-    Points p1 :
-      <input
-        value={''}
-        onChange={handlePointChange1}
-      />
-      <br />
-      Points p2 :
-      <input
-        value={''}
-        onChange={handlePointChange2}
-      />
-      <button type="submit">Add player</button>
+      {pointFields.map((pointField, index) => (
+        <div key={index} >
+          <TextField
+            name="p1"
+            label="p1"
+            variant="filled"
+            value={pointField.p1}
+            onChange={event => handleChange(index, event)}
+          />
+          <TextField
+            name="p2"
+            label="p2"
+            variant="filled"
+            value={pointField.p2}
+            onChange={event => handleChange(index, event)}
+          />
+
+          <IconButton onClick={handleAddFields}          >
+            <DoneIcon />
+          </IconButton>
+          <IconButton disabled={pointFields.length === 1} onClick={() => handleRemoveFields(index)}>
+            <DeleteIcon />
+          </IconButton>
+        </div>
+      ))}
+      <button type="submit">Tallenna</button>
     </form>
   )
 }
